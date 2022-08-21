1907c3fc3ab158cbf6d1b771299732082297747b
GKB <gkbm2013@gmail.com>
2022-08-21 19:04:31 +0800

    fix nav bar activation

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -12,10 +12,10 @@
                     <!--<li><a href="#">Help us for translation!</a></li>-->
                 </ul>
             </li>
-            <li :class="pathname == '/'? 'active' : ''"><a :href="localePath('index') + '/#home'">{{ $t("nav_home") }}</a></li>
+            <li :class="pageName == 'index'? 'active' : ''"><a :href="localePath('index') + '/#home'">{{ $t("nav_home") }}</a></li>
             <li><a :href="localePath('index') + '#features'">{{ $t("nav_features") }}</a></li>
             <li><a :href="localePath('index') + '#authors'">{{ $t("nav_authors") }}</a></li>
-            <li :class="pathname == '/downloads'? 'active' : ''"><a :href="localePath('downloads')">{{ $t("nav_downloads") }}</a></li>
+            <li :class="pageName == 'downloads'? 'active' : ''"><a :href="localePath('downloads')">{{ $t("nav_downloads") }}</a></li>
             <li><a href="https://github.com/gkbm2013/tinker-IO" target="_blank">{{ $t("nav_github") }}</a></li>
         </ul>
     </nav>
@@ -27,10 +27,12 @@ export default {
         light: Boolean
     },
     data() {
-        return {pathname: ""}
+        return {
+            pageName: ""
+        }
     },
-    mounted() {
-        this.pathname = window.location.pathname;
-    }
+    created() {
+        this.pageName = this.$nuxt.$route.name.split("___")[0];
+    },
 };
 </script>
