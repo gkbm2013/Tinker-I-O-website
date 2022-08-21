0b3d577834e7a65f3ddbeb57ad24f9f58d929fda
GKB <gkbm2013@gmail.com>
2022-08-21 18:00:53 +0800

    update links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,15 +7,15 @@
                     <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a href="?lang=en_US">English</a></li>
-                    <li><a href="?lang=zh_TW">繁體中文 (臺灣/中華民國)</a></li>
+                    <li><a :href="switchLocalePath('en')">English</a></li>
+                    <li><a :href="switchLocalePath('zh_tw')">繁體中文 (臺灣/中華民國)</a></li>
                     <!--<li><a href="#">Help us for translation!</a></li>-->
                 </ul>
             </li>
-            <li :class="pathname == '/'? 'active' : ''"><a href="/#home">{{ $t("nav_home") }}</a></li>
-            <li><a href="/#features">{{ $t("nav_features") }}</a></li>
-            <li><a href="/#authors">{{ $t("nav_authors") }}</a></li>
-            <li :class="pathname == '/downloads'? 'active' : ''"><a href="downloads">{{ $t("nav_downloads") }}</a></li>
+            <li :class="pathname == '/'? 'active' : ''"><a :href="localePath('index') + '/#home'">{{ $t("nav_home") }}</a></li>
+            <li><a :href="localePath('index') + '#features'">{{ $t("nav_features") }}</a></li>
+            <li><a :href="localePath('index') + '#authors'">{{ $t("nav_authors") }}</a></li>
+            <li :class="pathname == '/downloads'? 'active' : ''"><a :href="localePath('downloads')">{{ $t("nav_downloads") }}</a></li>
             <li><a href="https://github.com/gkbm2013/tinker-IO" target="_blank">{{ $t("nav_github") }}</a></li>
         </ul>
     </nav>
