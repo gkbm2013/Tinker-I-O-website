ec6aa15c2c162fa52f057f4b519a59e733c05284
GKB <gkbm2013@gmail.com>
2022-08-22 09:55:43 +0800

    fix home url

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -12,7 +12,7 @@
                     <!--<li><a href="#">Help us for translation!</a></li>-->
                 </ul>
             </li>
-            <li :class="pageName == 'index'? 'active' : ''"><a :href="localePath('index') + '/#home'">{{ $t("nav_home") }}</a></li>
+            <li :class="pageName == 'index'? 'active' : ''"><a :href="localePath('index') + '#home'">{{ $t("nav_home") }}</a></li>
             <li><a :href="localePath('index') + '#features'">{{ $t("nav_features") }}</a></li>
             <li><a :href="localePath('index') + '#authors'">{{ $t("nav_authors") }}</a></li>
             <li :class="pageName == 'downloads'? 'active' : ''"><a :href="localePath('downloads')">{{ $t("nav_downloads") }}</a></li>
